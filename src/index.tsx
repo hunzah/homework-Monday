40c894a04b730a65e89a6bd51343cefed8e5517e
Yusup <hunzah9@mail.ru>
2023-04-29 18:16:41 +0300

    hw5

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,19 @@ import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 import store from './s2-homeworks/hw10/bll/store'
 import {Provider} from 'react-redux'
-import {dividerClasses} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
-        <Provider store={store}>
-            <App/>
-        </Provider>
+
+            <Provider store={store}>
+                <App/>
+            </Provider>
+
     </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
 
 
