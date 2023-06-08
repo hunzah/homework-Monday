a9f8cfc8c9661677317288a5d696690632d93b65
Yusup <hunzah9@mail.ru>
2023-06-08 18:24:17 +0300

    hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+import s from './SuperRange.module.css'
 
-const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+
+
+type PropsType = {
+    value: number
+    oldValue?: number
+    change: (value: number | number[]) => void
 }
+
+const SuperRange: React.FC<SliderProps & PropsType> = (props) => {
+
+    function onChangeHandler(event: Event, newValue: number | number[]) {
+        const value = typeof newValue === 'number' ? newValue : newValue[1];
+        props.oldValue ?
+            props.change([value, props.oldValue])
+            : props.change(value);
+    }
+
+    return (
+            <Slider
+            sx={{
+                /* стили для слайдера */
+
+                width: 147,
+                color: '#01CB22',
+
+                '& .MuiSlider-thumb': {
+                    backgroundColor: 'white',
+                    border: '1px solid #01CB22',
+                    '&::before': {
+
+                        width: '5px',
+                        height: '6px',
+                        backgroundColor: '#01CB22',
+                        borderRadius: '50%',
+                    }
+                },
+            }}
+            {...props}
+            value={props.oldValue ? [props.oldValue, props.value] : props.value}
+            onChange={onChangeHandler}
+        />
+    );
+};
 
 export default SuperRange
